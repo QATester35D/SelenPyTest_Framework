09d7c3b1245b1f467f167986bcd8387d77e3c595
Shawn LoPorto <sloporto@asi-test.com>
2025-04-11 15:51:21 -0600

    Refactor test_api_calls.py: update class name to TestAPIGetCall, improve import statements, and comment out unused PUT request code

--- QATests/tests/test_api_calls.py
+++ QATests/tests/test_api_calls.py
@@ -1,8 +1,12 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
 import requests
 import pytest
 from QATests.tests.base_test import BaseTest
 
-class APIGetCall(BaseTest):
+class TestAPIGetCall(BaseTest):
 
     @pytest.mark.functional
     def test_get_api_call(self):
@@ -21,32 +25,3 @@ class APIGetCall(BaseTest):
 
         assert response.status_code == 200, "Status code is not 200"
 
-
-
-
-# This works:
-# head = {
-#     'Accept':'text/plain'
-# }
-
-# response = requests.get("https://fakerestapi.azurewebsites.net/api/v1/Activities/30",headers=head)
-# print(response.status_code)
-# print(response.json())
-
-# assert response.status_code == 200, "Status code is not 200"
-
-head = {
-    'Accept': 'text/plain',
-    'Content-Type': 'application/json'
-}
-
-putPayLoad = {
-  "id": 30,
-  "title": "shawn test123_v2",
-  "dueDate": "2025-04-10T21:24:19.546Z",
-  "completed": True
-}
-
-response = requests.put("https://fakerestapi.azurewebsites.net/api/v1/Activities/30", headers=head, json=putPayLoad)
-print(response.status_code)
-print(response.json())

f14810248b2fc9eec2b42f538078b346ea268d49
Shawn LoPorto <sloporto@asi-test.com>
2025-04-15 16:11:55 -0600

    Refactor test cases and update report generation
    - Updated AutomationPyTestReport.html to reflect no tests run and adjusted timestamps.
    - Removed unnecessary imports and commented-out code in test_api_calls.py and test_api_calls_users.py.
    - Added new test cases for form submissions using data from CSV and JSON files in test_web_form csvFile.py and test_web_form jsonFile.py.
    - Implemented parameterized tests in test_parameters.py and test_params_multiple_browsers.py.
    - Created new test files for assertions and markers in the DemoPytest_NoPOM directory.
    - Deleted obsolete test_launchSites.py and its compiled files.
    - Updated base_page.py to remove unused imports.

--- QATests/API/test_api_calls_users.py
+++ QATests/API/test_api_calls_users.py
@@ -1,7 +1,3 @@
-# import sys
-# import os
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import uuid
 import pytest
 from QATests.utilities.api_client import APIClient
@@ -20,11 +16,6 @@ class TestAPICalls:
 
     #This is failing because the website isn't allowing Posts, the post executes successfully but doesn't actually store any data
     def test_create_user(self, test_api_client, load_api_user_data):
-        # user_data = {
-        #     "name": "John Doe",
-        #     "username": "qa user",
-        #     "email": "test@gmail.com"
-        # }
         user_data = load_api_user_data["new_user"]
         unique_email = f"{uuid.uuid4().hex[:8]}@gmail.com"
         print(unique_email)
@@ -34,7 +25,8 @@ class TestAPICalls:
         assert response.status_code == 201, "Failed to get users"
         assert response.json()['name'] == "shawn", "User name does not match"
         id = response.json()['id']
-        # This will fail because the data from above was mocked on the API server side and not actually created
+        # This is the correct approach but it will fail because the data from above 
+        #    was mocked on the API server side and not actually created on their side
         # response_get = test_api_client.get(f"users/{id}")
         # print(response_get.json())
         # assert response_get.status_code == 200, "Failed to get users"

7a72399ef16f39b61f1a7c7bb53b325d9b832e6f
Shawn LoPorto <sloporto@asi-test.com>
2025-04-16 11:42:47 -0600

    Remove unused increment/decrement functions and associated test cases; update README and pytest configuration

--- AddSubCode/inc_dec.py
+++
@@ -1,8 +0,0 @@
-def increment(x):
-    return x + 1
-
-def decrement(x):
-    return x - 1
-
-def decrementByTwo(x):
-    return x - 2
--- QATests/AddSubUnitTests/test_pytest.py
+++
@@ -1,9 +0,0 @@
-import AddSubCode.inc_dec as inc_dec    # The code to test
-
-def test_increment():
-    assert inc_dec.increment(3) == 4
-
-# This test is designed to fail for demonstration purposes.
-def test_decrement():
-    # assert inc_dec.decrement(3) == 4 #intended to fail to show how pytest works
-    assert inc_dec.decrement(3) == 2

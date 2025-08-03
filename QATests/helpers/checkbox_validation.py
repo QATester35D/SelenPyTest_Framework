fa0c615fb26be0bdfa99598fd424e70fd39edd5b
Shawn LoPorto <sloporto@asi-test.com>
2025-08-03 08:29:12 -0600

    Fix formatting issues in checkbox validation assertions for consistency

--- QATests/helpers/checkbox_validation.py
+++ QATests/helpers/checkbox_validation.py
@@ -38,11 +38,11 @@ class CheckboxValidation(BasePage):
 
         for name, check in checky_checkboxes.items():
             checkbox = self.find(*check["locator"])
-            self.assert_checkbox_value(checkbox.is_selected(),check["expected_selected"],f"{name} checkbox, verifying the 'selected' state. Expected: {check['expected_selected']}, Actual is: {checkbox.is_selected()}", assert_helper)
-            self.assert_checkbox_value(checkbox.is_displayed(),check["expected_display"],f"{name} checkbox, verifying the 'displayed' state. Expected: {check['expected_display']}, Actual is: {checkbox.is_displayed()}", assert_helper)
-            self.assert_checkbox_value(checkbox.is_enabled(),check["expected_enabled"],f"{name} checkbox, verifying the 'enabled' state. Expected: {check['expected_enabled']}, Actual is: {checkbox.is_enabled()}", assert_helper)
+            self.assert_checkbox_value(checkbox.is_selected(),check['expected_selected'],f"{name} checkbox, verifying the 'selected' state. Expected: {check['expected_selected']}, Actual is: {checkbox.is_selected()}", assert_helper)
+            self.assert_checkbox_value(checkbox.is_displayed(),check['expected_display'],f"{name} checkbox, verifying the 'displayed' state. Expected: {check['expected_display']}, Actual is: {checkbox.is_displayed()}", assert_helper)
+            self.assert_checkbox_value(checkbox.is_enabled(),check['expected_enabled'],f"{name} checkbox, verifying the 'enabled' state. Expected: {check['expected_enabled']}, Actual is: {checkbox.is_enabled()}", assert_helper)
             if name == "checky":
-                self.assert_checkbox_value(checkbox.get_attribute("value"),check["expected_value"],f"{name} checkbox, verifying the 'value' attribute. Expected: {check["expected_value"]}, Actual is: {checkbox.get_attribute("value")}", assert_helper)
+                self.assert_checkbox_value(checkbox.get_attribute("value"),check['expected_value'],f"{name} checkbox, verifying the 'value' attribute. Expected: {check['expected_value']}, Actual is: {checkbox.get_attribute('value')}", assert_helper)
 
     def assert_checkbox_value(self, actual_title, expected_title, message, assert_helper):
         assert_helper.equal(

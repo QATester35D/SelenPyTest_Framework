8f027d1766aca232d61bad9c5031c9b65e3c2495
Shawn LoPorto <sloporto@asi-test.com>
2025-08-23 10:24:25 -0600

    Refactor TestSakilaDatabaseOperations to inherit from BaseTest and update test_customer_shipping_address method to use setup_database

--- QATests/tests/test_customer_orders.py
+++ QATests/tests/test_customer_orders.py
@@ -1,7 +1,10 @@
+import pytest
 import mysql.connector
+from QATests.tests.base_test import BaseTest
 from QATests.db.db_config import DB_CONFIG_SAKILA
+from QATests.db.db_connection import DatabaseOperations
 
-class TestSakilaDatabaseOperations:
+class TestSakilaDatabaseOperations(BaseTest):
     def __init__(self):
         self.mydb = mysql.connector.connect(**DB_CONFIG_SAKILA)
         self.cur = self.mydb.cursor()
@@ -12,8 +15,9 @@ class TestSakilaDatabaseOperations:
         result = cursor.fetchall()
         return result
 
-    def test_customer_shipping_address(self):
-        cursor = self.cur()
+    def test_customer_shipping_address(self, setup_database: DatabaseOperations):
+        db_helpers = setup_database
+        cursor = db_helpers.cur
         query = "select first_name from customer where city_id = 463;"
         cursor.execute(query)
         result = cursor.fetchone()[0]
